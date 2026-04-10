2cf9e46ff41c59a4915c6bcbbe70343cb4175b67
dimgunner <dimgunner@gmail.com>
2026-04-10 11:57:07 +0300

    [FIX] fct_portal_voice_message: check if _set_voice metadata() exists

--- fct_portal_voice_message/controllers/portal.py
+++ fct_portal_voice_message/controllers/portal.py
@@ -11,9 +11,13 @@ class CustomerPortal(portal.CustomerPortal):
     def attachment_add(self, name, file, res_model, res_id, access_token=None, **kwargs):
         res = super().attachment_add(name, file, res_model, res_id, access_token, **kwargs)
 
-        if kwargs.get("voice"):
+        if kwargs.get('voice'):
             attachment = request.env['ir.attachment'].sudo().browse(res.json.get('id'))
-            attachment._set_voice_metadata()
+
+            if hasattr(attachment, '_set_voice_metadata'):
+                attachment._set_voice_metadata()
+            else:
+                request.env['discuss.voice.metadata'].sudo().create([{'attachment_id': attachment.id}])
 
             data = res.json.copy()
             data['voice'] = 1
